3a3074ef8d67c2952a5b9902084b09da8d8702a9
Phuc Le <info@phuc.at>
2020-03-05 15:24:53 +0100

    fixed missing content

--- src/block/block.js
+++ src/block/block.js
@@ -12,7 +12,7 @@ import { InnerBlocks } from '@wordpress/block-editor';
 
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
-const ALLOWED_BLOCKS = [ 'core/image', 'core/paragraph' ];
+// const ALLOWED_BLOCKS = [ 'core/image', 'core/paragraph' ];
 
 const TEMPLATE = [ 
 	[ 
@@ -88,8 +88,7 @@ registerBlockType( 'cgb/block-wtp-plugin-block', {
 			<div className={ props.className }>
 				<InnerBlocks
 					template={ TEMPLATE }
-					templateLock='all'
-					// allowedBlocks={ ALLOWED_BLOCKS }
+					// templateLock='all'
 				/>
 			</div>
 		);
